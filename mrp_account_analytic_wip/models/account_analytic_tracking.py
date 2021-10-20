6181e7dd3d14be447d7eaee8d88a66563b634c80
Daniel Reis <dreis.pt@hotmail.com>
2021-10-20 21:34:41 +0100

    [FIX] mrp_account_analytic_wip: close MO and clear WIP process

--- mrp_account_analytic_wip/models/account_analytic_tracking.py
+++ mrp_account_analytic_wip/models/account_analytic_tracking.py
@@ -50,6 +50,7 @@ class AnalyticTrackingItem(models.Model):
         # Only set for raw materials
         if dest_location.valuation_in_account_id:
             accounts["stock_input"] = dest_location.valuation_in_account_id
+            accounts["stock_wip"] = accounts["stock_input"]
         if dest_location.valuation_out_account_id:
             accounts["stock_output"] = dest_location.valuation_out_account_id
         if dest_location.valuation_variance_account_id:
