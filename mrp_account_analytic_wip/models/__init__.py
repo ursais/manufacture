fc42426664da25a096c4da65cdf4464c79ce7fcb
Daniel Reis <dreis@opensourceintegrators.com>
2023-06-08 12:18:59 +0100

    [IMP] mrp_account_analytic_wip: Update Tracking Items based on the Reference BoM
    This creates/updates Tracking Items based on the Reference Bom.
    It is triggered on Action Confirm.
    
    Not implemented gaps:
    - Update Tracking Items upon Reference BOM change.
    - Update Tracking Items when Work Center is changed in a work order

--- mrp_account_analytic_wip/models/__init__.py
+++ mrp_account_analytic_wip/models/__init__.py
@@ -1,5 +1,6 @@
 from . import account_analytic_line
 from . import account_analytic_tracking
+from . import mrp_bom
 from . import mrp_production
 from . import mrp_workcenter
 from . import mrp_workorder
