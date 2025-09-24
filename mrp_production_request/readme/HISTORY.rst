e9e3ceddc3320ca19ddf0957a38da52cbf8cc237
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-09-24 17:35:50 +0000

    Fix for ticket 66572: Submodule update
    Automated changes for ticket resolution

--- mrp_production_request/readme/HISTORY.rst
+++
@@ -1,4 +0,0 @@
-11.0.1.0.0 (2018-09-13)
-~~~~~~~~~~~~~~~~~~~~~~~
-
-* [MIG] Migration to v11. Start of the history.
--- quality_control_plan/models/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from . import mgmtsystem_nonconformity_inspection, partner_quality, \
-                qc_inspection_plan, qc_plan, qc_trigger, stock_picking_inspection
